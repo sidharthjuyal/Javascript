c0f9aae330b6e1fffc20e8b6cf06ffbaca55775f
Sidharth juyal <sid.juyal.34@gmail.com>
2022-04-04 23:43:22 +0530

    Create intro.js

---
+++ intoduction/intro.js
@@ -0,0 +1,17 @@
+console.log("Introduction to javascript");
+
+/*
+
+JAVASCRIPT
+
+-> client side scripting language.
+-> used to make web pages alive.
+-> originally called 'livescript'
+-> programatically performs actions within a page. 
+-> can also be used in server side scripting.
+-> used in browser as well as servers.
+-> HTML/CSS support
+-> gives simple API's for simple functionalities.
+-> supports major modern browser(by default open in them)
+
+*/
