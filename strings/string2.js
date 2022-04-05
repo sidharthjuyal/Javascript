04e760c3ba8cd76c3ca896c6b65992f9c8afde91
Sidharth juyal <sid.juyal.34@gmail.com>
2022-04-05 20:04:22 +0530

    Create string2.js

---
+++ strings/string2.js
@@ -0,0 +1,26 @@
+var Name="Sidharth";
+
+// string length
+var len=Name.length;
+console.log("The length of name is: "+len);
+
+/*
+some escape sequences
+
+-> \n  new line
+-> \\  backslash
+-> \t  tab
+-> \'  single quote
+-> \"  double quotes
+-> \b  backspace
+*/
+
+console.log('This is my brother\'s wish \n Iam Sidharth \t How are you?');
+
+// making a string using a string constructor
+var y = new String("hello!");
+console.log(y);
+
+
+
+
