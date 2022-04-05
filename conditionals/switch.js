ad40ccede72d697cb76cc2635a547fa3b4610b84
Sidharth juyal <sid.juyal.34@gmail.com>
2022-04-05 20:34:45 +0530

    Create switch.js

---
+++ conditionals/switch.js
@@ -0,0 +1,19 @@
+let day=1;
+switch(day)
+{
+    case 1: console.log("Today is Monday");
+            break;
+    case 2: console.log("Today is Tuesday");
+            break;
+    case 3: console.log("Today is Wednesday");
+            break;
+    case 4: console.log("Today is Thursday");
+            break;
+    case 5: console.log("Today is Friday");
+            break;
+    case 6: console.log("Today is Saturday");
+            break;
+    case 7: console.log("Today is Sunday");
+            break;
+    default: console.log("enter a valid choice.");
+}
