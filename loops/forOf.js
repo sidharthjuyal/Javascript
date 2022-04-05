c755b00feb977df84c7b1f02bf91e4e10dc5cc89
Sidharth juyal <sid.juyal.34@gmail.com>
2022-04-06 00:52:07 +0530

    Loops

---
+++ loops/forOf.js
@@ -0,0 +1,8 @@
+let friends=["Sid","abhi","akash","sushant"];
+
+// iterating an array using for-of loop
+for(element of friends)
+{
+    console.log("Hello friend in forOf loop: " + element);    
+}
+console.log("\n");
