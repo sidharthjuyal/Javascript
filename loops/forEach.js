35aabdd410b0903e1f8a8db81a7b1b9fd94aff06
Sidharth juyal <sid.juyal.34@gmail.com>
2022-04-06 00:51:37 +0530

    Loops

---
+++ loops/forEach.js
@@ -0,0 +1,7 @@
+let friends=["Sid","abhi","akash","sushant"];
+
+// iterating an array using forEach
+friends.forEach(function f(element){
+    console.log("Hello friend in forEach loop: " + element);    
+});
+console.log("\n");
