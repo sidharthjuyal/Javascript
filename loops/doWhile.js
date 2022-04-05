4f80b5ad77ff4d87eef2f1e7f51b88a47c75f9bc
Sidharth juyal <sid.juyal.34@gmail.com>
2022-04-06 00:51:18 +0530

    Loops

---
+++ loops/doWhile.js
@@ -0,0 +1,20 @@
+// do-while loop
+let i=4;
+
+do{
+console.log(`${i}: see? output is being printed atleast once`);
+i++;
+}while(i<4);
+console.log("\n");
+
+
+// while loop in case of array
+let friends=["Sid","abhi","akash","sushant"];
+
+let j=0;
+do
+{
+    console.log(`${friends[j]} is my friend.`);
+    j++;
+}
+while(j<friends.length);
