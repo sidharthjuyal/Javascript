047ebb488b9749f0fefe9feff2166c11538fb19d
Sidharth juyal <sid.juyal.34@gmail.com>
2022-04-05 20:03:11 +0530

    Create operators.js

---
+++ basic/operators.js
@@ -0,0 +1,31 @@
+var a = 69;
+var b = "siddhu";
+var d = " juyal";
+c = 30.55;
+
+/*
+operators in js
+-> unary - has single operand
+-> binary - has two operands
+
+operands
+->entities on which operators operate
+*/
+
+//binary operators
+console.log(a - c);
+console.log(b + d);
+
+//unary operators
+console.log(-c);
+console.log(c++);
+console.log(c);
+//see 
+console.log(++a);
+
+var num1 = 69;
+var num2 = 420;
+console.log("The value of num1 + num2 is  " + (num1 + num2));
+console.log("The value of num1 - num2 is  " + (num1 - num2));
+console.log("The value of num1 * num2 is  " + (num1 * num2));
+console.log("The value of num1 / num2 is  " + (num1 / num2));
