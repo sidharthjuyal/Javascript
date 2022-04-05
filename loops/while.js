8b2a86ff71f6a23b3e42fd67255d58026e32b634
Sidharth juyal <sid.juyal.34@gmail.com>
2022-04-06 00:52:21 +0530

    Loops

---
+++ loops/while.js
@@ -0,0 +1,19 @@
+// while loop
+let i=0;
+while(i<4)
+{
+    console.log(`${i} is less than 4`);
+    i++;
+}
+console.log("\n");
+
+
+// while loop in case of array
+let friends=["Sid","abhi","akash","sushant"];
+
+let j=0;
+while(j<friends.length)
+{
+    console.log(`${friends[j]} is my friend.`);
+    j++;
+}
