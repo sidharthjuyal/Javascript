882931b2941bc8f36bd29fb64896126a7310f668
Sidharth juyal <sid.juyal.34@gmail.com>
2022-04-05 20:04:05 +0530

    Create string1.js

---
+++ strings/string1.js
@@ -0,0 +1,17 @@
+// you can use single or double quotes
+var string1="string";
+console.log(string1);
+
+// if you want to print double quotes
+var string2='"My name is Sid';
+var string3=' and iam a kid"';
+console.log(string2+string3);
+
+
+var Name="Sidharth";
+var channel="Winter Nights";
+
+// how to make a string template
+// you can use both single and double quotes in this
+var temp=`${Name} is a 'nice' person "and" he has channel namely ${channel}`;
+console.log(temp);
