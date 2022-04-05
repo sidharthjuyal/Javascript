ac0242803856830e011cd4e710761d9c3abfcd11
Sidharth juyal <sid.juyal.34@gmail.com>
2022-04-06 00:51:05 +0530

    Loops

---
+++ loops/forLoop.js
@@ -0,0 +1,18 @@
+// for loop
+let i = 0;
+
+for (i = 0; i < 3; i++) 
+{
+    console.log(i);
+}
+console.log("\n");
+
+
+// iterating an array
+let friends=["Sid","abhi","akash","sushant"];
+
+for (let i = 0; i < friends.length; i++) 
+{
+    console.log("Hello friend: " + friends[i]);    
+}
+console.log("\n");
